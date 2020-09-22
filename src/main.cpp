19dde2eb57ab779600214824c8f23b9017d6524f
Sebastian Roman <sromandavila@icloud.com>
2020-09-22 08:52:10 -0400

    Pre-Season Project Demo

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+  
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	double num1, num2, answer;
+	char choice;
+
+	cout << "Enter 2 numbers, press enter in-between the first and second." << endl;
+	cin >> num1;
+	cin >> num2;
+
+	cout << "Now enter the requested operation for the 2 numbers (+, -, *, /, %)" << endl;
+	cin >> choice;
+
+	if (choice == '+')
+		answer = num1 + num2;
+	else if (choice == '-')
+		answer = num1 - num2;
+	else if (choice == '*')
+		answer = num1 * num2;
+	else if (choice == '/')
+		answer = num1 / num2;
+	else if (choice == '%') //% dividing only works with integers, so we need to convert
+		answer = int(num1) % int(num2); //The int() function converts the data type to integer
+	else
+		choice = 'i';
+
+	if (choice == 'i')
+		cout << "Invalid operation, restart the program." << endl;
+	else
+		cout << num1 << choice << num2 << "=" << answer << endl;
