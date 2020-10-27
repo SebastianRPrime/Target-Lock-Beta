394cb5af67329017faf4f297976dc16e6265d56c
Sebastian Roman <sromandavila@icloud.com>
2020-10-27 14:22:58 -0400

    Added and fixed file location

---
+++ Preseason-Projects/BeginnersOutputAndInput.cpp
@@ -0,0 +1,12 @@
+//First Example practice
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string name;
+  cout << "What is your name? " << endl;
+  cin >> name;
+  cout << "Hello, " << name << "!\n";
+}
